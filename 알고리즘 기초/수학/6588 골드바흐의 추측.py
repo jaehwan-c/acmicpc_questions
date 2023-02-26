ef8a4058d272246fbdce739e16781be9f129c7cd
jaehwan-c <jhchoi75@hku.hk>
2023-02-26 17:02:30 +0800

    6588 fix error

--- 알고리즘 기초/수학/6588 골드바흐의 추측.py
+++ 알고리즘 기초/수학/6588 골드바흐의 추측.py
@@ -1,19 +1,29 @@
+import sys
+
 max_val = 1000000
 
 dp = [1] * (max_val + 1)
+prime_num = list()
+dp_dic = {}
 
-for i in range(3, 1001):
-    for j in range(i + i, max_val + 1, i):
-        dp[j] = 0
+for i in range(2, 1001):
+    if dp[i]:
+        for j in range(i + i, max_val + 1, i):
+            dp[j] = 0
+            
+for i in range(3, max_val + 1):
+    if dp[i] == 1:
+        prime_num.append(i)
+        dp_dic[i] = 1
 
 while True:
-    n = int(input())
+    n = int(sys.stdin.readline())
     
     if n == 0:
         break
     
     else:
-        for i in range(3, len(dp), 2):
-            if dp[i] and dp[n-i]:
-                print("%d = %d + %d" %(n, i, n-i))
+        for i in range(len(prime_num)):
+            if (n - prime_num[i] in dp_dic):
+                print("%d = %d + %d" %(n, prime_num[i], n-prime_num[i]))
                 break
